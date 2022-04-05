de6363d747e0e0b3b897abadfea5b40033b11d2d
AEVeNuz <99236401+AEVeNuz@users.noreply.github.com>
2022-04-04 23:25:33 -0600

    Primer commit
    practica con blazor

---
+++ ProyectoBlazor/Modelos/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelos
+{
+    public class Producto
+    {
+        [Required(ErrorMessage = "El Campo Código es Obligatorio")]
+        public string Codigo { get; set; }
+        [Required(ErrorMessage = "El Campo Descripción es Obligatorio")]
+        public string Descripcion { get; set; }
+        public int Precio { get; set; }
+        public int Existencia { get; set; }
+    }
+}
